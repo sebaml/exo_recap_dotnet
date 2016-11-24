42f9b7469741feafbc28e37fe6990a1719577f1f
Sebastien Martin Leon <sebastien.martinleon@hermes-ecs.com>
2016-11-24 16:23:26 +0100

    New architecture version with full supplier webservices

--- Contoso/DataModel/SupplierType.cs
+++ Contoso/DataModel/SupplierType.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
-namespace DataRequest
+namespace DataModel
 {
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     [DataContract]
--- Contoso/ServicesLayer/SupplierService.cs
+++ Contoso/ServicesLayer/SupplierService.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using DataModel;
 using DataRequest;
-
 
 namespace ServicesLayer
 {
     public class SupplierService : ISupplierService
     {
-
-
         public List<SupplierType> GetSupplierByName(string name)
         {
             return SupplierRequests.ReqGetSupplierByName(name);
